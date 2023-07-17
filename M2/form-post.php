7f7fcc4a98b6b58e1e480a4b1e9c074e7f3d17da
taitamfc <taitam8590@gmail.com>
2023-07-17 16:16:10 +0700

    form

---
+++ M2/form-post.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<form action="" method="post">
+    Ten dang nhap: <input type="text" name="usernmae" > <br>
+    Mat khau: <input type="password" name="password" > <br>
+    <input type="submit" value="Dang nhap">
+</form>
