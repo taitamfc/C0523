da5d6a0b9aeeff800bfcdb7b2ebd7557927cc1ed
taitamfc <taitam8590@gmail.com>
2023-07-17 16:33:58 +0700

    form-post

--- M2/form-post.php
+++ M2/form-post.php
@@ -1,19 +1,35 @@
 <?php
+    // $_SERVER: thông tin máy chủ
+    // $_POST  : nhận dữ liệu gửi lên qua method POST
+    // $_GET   : nhận dữ liệu gửi lên qua method GET
+    // $_REQUEST : nhận dữ liệu gửi lên qua method GET
 
-    $name = 'NVA';
-    echo $name;
+    if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
+        // Xử lý
+        // Lấy giá trị gửi lên
+        echo '<pre>';
+        print_r($_REQUEST);
+        echo '</pre>';
 
-    $books = ['Van','Su','Dia'];
+        // Lưu trữ
+        $ho_va_ten = $_REQUEST['ho_va_ten'];
+        $mat_khau = $_REQUEST['mat_khau'];
 
-    echo '<pre>';
-    print_r($books);
-    echo '</pre>';
+        // Xử lý
+        if( $ho_va_ten == 'admin' && $mat_khau == '123456' ){
+            echo 'Chao mung admin';
+        }else{
+            echo 'Tai khoan khong dung';
+        }
+        
+    }
+    // echo '<pre>';
+    // print_r($_SERVER);
+    // echo '</pre>';
 
-
-    
 ?>
 <form action="" method="post">
-    Ten dang nhap: <input type="text" name="usernmae" > <br>
-    Mat khau: <input type="password" name="password" > <br>
+    Ten dang nhap: <input type="text" name="ho_va_ten" > <br>
+    Mat khau: <input type="password" name="mat_khau" > <br>
     <input type="submit" value="Dang nhap">
 </form>
---
+++ M2/output.php
@@ -0,0 +1,11 @@
+<?php
+
+    $name = 'NVA';
+    echo $name;
+
+    $books = ['Van','Su','Dia'];
+
+    echo '<pre>';
+    print_r($books);
+    echo '</pre>';
+?>
