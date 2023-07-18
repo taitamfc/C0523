8e77e6c97c973a4bab832e7276f0a18c72ffe415
taitamfc <taitam8590@gmail.com>
2023-07-18 09:04:42 +0700

    HCN prepare

---
+++ M2/002/HCN.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    B1: tao form:post gom 2 o input + name, va 1 nut submit
+    B2: Kiem tra nguoi dung da gui du lieu len server
+        - In ra du lieu nguoi dung da gui len
+    B3: Gan vao cac bien: $_POST, $_REQUEST
+    B4: Xu ly tinh chu vi va dien tich HCN
+    B5: Hien thi ra ket qua
+*/
+?>
