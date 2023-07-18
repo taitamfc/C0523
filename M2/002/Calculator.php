9c20e87bde516ae8338fefc24ae524082c8b1db3
taitamfc <taitam8590@gmail.com>
2023-07-18 08:34:43 +0700

    Calculator prepare

---
+++ M2/002/Calculator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    B1: tao form:post gom 3 o input + name, va 1 nut submit
+    B2: Kiem tra nguoi dung da gui du lieu len server
+        - In ra du lieu nguoi dung da gui len
+    B3: Gan vao cac bien: $_POST, $_REQUEST
+    B4: Su dung cau truc nhieu dieu kien de xu ly
+    B5: Hien thi ra ket qua
+*/
+?>
